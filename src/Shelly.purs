95cdd6a9a7e6a437a1bf3a8d332fe3e775b5c355
Felix Schlitter <felixschlitter@gmail.com>
2016-04-12 13:17:10 +1200

    Read program output into stream of lines

--- src/Shelly.purs
+++ src/Shelly.purs
@@ -7,8 +7,11 @@ import Data.Tuple (Tuple(..))
 import Control.Monad (when, unless)
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Unsafe (unsafeInterleaveEff)
+import Control.Monad.Aff.Unsafe (unsafeInterleaveAff)
 import Control.Bind ((>=>), (=<<))
 import Control.Monad.Aff
+import Control.Monad.Aff.AVar as AVar
+import Control.Monad.Aff.AVar (AVAR)
 import Control.MonadPlus (guard)
 import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Eff.Class (liftEff)
@@ -29,7 +32,13 @@ import Control.Monad.Eff.Exception (Error(), EXCEPTION, throwException, error)
 import Control.Monad.Error.Class (throwError, catchJust)
 import Node.ChildProcess as ChildProcess
 import Node.ChildProcess as Exit
-import Node.ChildProcess (CHILD_PROCESS)
+import Node.ChildProcess (ChildProcess(), CHILD_PROCESS)
+import Node.Stream as Stream
+import Node.Buffer as Buffer
+import Node.ReadLine as ReadLine
+import Node.ReadLine (READLINE)
+import Node.Encoding (Encoding(..))
+import Data.Monoid (mempty)
 
 foreign import code :: Error -> String
 
@@ -82,12 +91,27 @@ cd fp = do
               ++ " The directory does not exist: " ++ show dir)
   modify \st -> st { cwd = dir }
 
-run :: forall e. String -> Array String -> Sh (cp :: CHILD_PROCESS | e) Unit
+run :: forall e
+     . String
+    -> Array String
+    -> Sh ( cp       :: CHILD_PROCESS
+          , avar     :: AVAR
+          , readline :: READLINE
+          | e) Unit
 run cmd args = lift do
+  avar <- AVar.makeVar' []
   makeAff \reject resolve -> do
 
     proc <- liftEff do
       ChildProcess.spawn cmd args ChildProcess.defaultSpawnOptions
+
+
+    liftEff do
+      iface <- ReadLine.createInterface (ChildProcess.stdout proc)
+                                        mempty
+
+      ReadLine.setLineHandler iface \line -> do
+        traceA line
 
     ChildProcess.onExit proc \exit -> case exit of
       Exit.Normally 0 -> resolve unit
